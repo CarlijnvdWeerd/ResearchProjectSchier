93d4675061e02490ddd21264b6520d37c39f437f
Carlijn van der Weerd <c.van.der.weerd@student.rug.nl>
2025-06-26 20:22:25 +0200

    analysis of moult & fat

--- r-scripts/10_fat&molt.R
+++ r-scripts/10_fat&molt.R
@@ -92,8 +92,11 @@ PJC.21.05", "JHN.07.05", "KTM.16.04", "PJU_08.05", "PLH_20.05", "
 JJV.16.04", "KHK.01.05", "NPP.30.04", "JKL.29.04", "KKM_08.05", "NPY_08.05", "KKN.20.05", "NTV.10.04(2)", "NTV_24.04", "NTV.25.04", "JLO.28.03", "	
 KKT_16.04", "KXV.01.05", "NUK_24.04")))
 video_data$Date <- as.Date(video_data$Date, format = "%m/%d/%Y")
+molt_data <- video_data |>
+  filter(!(Three_letter_code %in% c("MAU", "PAE", "PNL", "PAJ", "LPT", "MTJ", "PKN", "MJA", "JHY", "KET", "PLA", "KKH", "KTP", "PVK", "LEA", "NUK", "NEL", "JJV", "KKT", "PJC")))
+# Removed birds that did not molt
 
-ggplot(video_data, aes(x = as.Date(Date), y = Molting_score, 
+ggplot(molt_data, aes(x = as.Date(Date), y = Molting_score, 
                        color = Strategy, group = Three_letter_code)) +
   geom_point() +
   geom_line() +
@@ -103,7 +106,7 @@ ggplot(video_data, aes(x = as.Date(Date), y = Molting_score,
   theme_minimal() +
   theme(legend.position = "bottom")
 
-ggplot(video_data, aes(x = Date, y = Molting_score, color = Strategy)) +
+ggplot(molt_data, aes(x = Date, y = Molting_score, color = Strategy)) +
   geom_point(alpha = 0.5) +  # Optional: lower point opacity
   geom_smooth(se = FALSE, method = "loess") +  # or method = "gam", "lm"
   labs(title = "Smoothed Molting Trends by Strategy",
@@ -112,7 +115,7 @@ ggplot(video_data, aes(x = Date, y = Molting_score, color = Strategy)) +
   theme_minimal() +
   theme(legend.position = "bottom")
 
-p_molt <- ggplot(video_data, aes(x = Date, y = Molting_score, color = Strategy, group = Three_letter_code)) +
+p_molt <- ggplot(molt_data, aes(x = Date, y = Molting_score, color = Strategy, group = Three_letter_code)) +
   geom_line(alpha = 0.3) +  # individual bird lines, faint
   geom_point(alpha = 0.3) +
   geom_smooth(aes(group = Strategy), se = FALSE, method = "loess", size = 1.2) +
@@ -125,7 +128,7 @@ p_molt <- ggplot(video_data, aes(x = Date, y = Molting_score, color = Strategy, 
 p_molt
 
 ###########################################################################
-
+## Need to make a new dataframe and not use video_data, because removed the birds that did not moult
 p_fat<-ggplot(video_data, aes(x = Date, y = Fat_score, color = Strategy, group = Three_letter_code)) +
   geom_line(alpha = 0.3) +  # individual bird lines, faint
   geom_point(alpha = 0.3) +
@@ -137,3 +140,162 @@ p_fat<-ggplot(video_data, aes(x = Date, y = Fat_score, color = Strategy, group =
   theme_minimal() +
   theme(legend.position = "bottom")
 p_fat
+
+########################################################################
+#### remove observations so last day with moult score 5, is the day they scored 5 for the first time.
+real_last_day <- molt_data |>
+  filter(!(Observation_id %in% c("KXM_14.05", "JYL_08.05", "MCH_21.05", "KPM.15.05", "KPM.20.05", "PMP.20.05", "JHM.21.05", "KMC_13.05", "KMC_14.05", "KMC.15.05", "KMC_20.05", "JAE_08.05", "JAE.14.05", "JAE_20.05", "	
+JLU.25.04", "KCY.21.05", "NKE.01.05", "NKE.14.05", "PHT_20.05", "JLH_20.05", "KVV_20.05", "NTV_14.05" )))
+
+# Calculate per-bird slope (molting change per day)
+molting_slopes <- real_last_day %>%
+  group_by(Three_letter_code) %>%
+  arrange(Date) %>%
+  summarise(
+    First_Date = first(Date),
+    Last_Date = last(Date),
+    First_Score = first(Molting_score),
+    Last_Score = last(Molting_score),
+    Slope = (Last_Score - First_Score) / as.numeric(Last_Date - First_Date),
+    .groups = "drop"
+  )
+
+slopes_with_strategy <- molting_slopes %>%
+  left_join(video_data %>% dplyr::select(Three_letter_code, Strategy) %>% distinct(), by = "Three_letter_code") |>
+  filter(!(Three_letter_code %in% c("KAX", "KKM", "KTN", "PJC")))
+## to remove birds that where only seen in breeding plumage 
+
+ggplot(slopes_with_strategy, aes(x = Strategy, y= Slope, fill = Strategy)) +
+  geom_boxplot() +
+  scale_fill_manual(values = c("#E777F2", "#4DD2A4", "#4DC8F9")) +
+  labs(title = "Distribution of Molting Slopes by Strategy",
+       x = "Slope (Molting Change per Day)",
+       y = "Density") +
+  theme_minimal() +
+  theme(legend.position = "bottom")
+
+ggplot(slopes_with_strategy, aes(x = Slope)) +
+  geom_histogram(bins = 30, fill = "#69b3a2", color = "black") +
+  theme_minimal()
+
+glm1 <- glm(Slope ~ 1,
+            data = slopes_with_strategy,
+            family = Gamma(link = "log"))
+
+glm2 <- glm(Slope ~ Strategy,
+            data = slopes_with_strategy,
+            family = Gamma(link = "log"))
+summary(glm2)
+
+model.sel(glm1, glm2)
+anova(glm1, glm2, test = "Chisq")
+
+glm3 <- glm(Slope ~ First_Date,
+            data = slopes_with_strategy,
+            family = Gamma(link = "log"))
+
+glm4 <- glm(Slope ~ First_Date + Strategy,
+            data = slopes_with_strategy,
+            family = Gamma(link = "log"))
+
+model.sel(glm1, glm2, glm3, glm4)
+
+glm5 <- glm(Slope ~ First_Date * Strategy,
+            data = slopes_with_strategy,
+            family = Gamma(link = "log"))
+
+glm6 <- glm(Slope ~ Last_Date,
+            data = slopes_with_strategy,
+            family = Gamma(link = "log"))
+
+glm7 <- glm(Slope ~ First_Date + Last_Date + Strategy,
+            data = slopes_with_strategy,
+            family = Gamma(link = "log"))
+summary(glm7)
+
+model.sel(glm1, glm2, glm3, glm4, glm5, glm6, glm7)
+anova(glm3, glm4, glm5, glm6, glm7, test = "Chisq")
+
+emm_molt <- emmeans(glm7, ~ Strategy, type = "response")
+pairs(emm_molt)
+library(multcomp)
+
+cld(emm_molt, Letters = letters, adjust = "tukey")
+
+############################################################################
+# Calculate per-bird slope (molting change per day)
+fatting_slopes <- video_data %>%
+  group_by(Three_letter_code) %>%
+  arrange(Date) %>%
+  summarise(
+    First_Date = first(Date),
+    Last_Date = last(Date),
+    First_Score = first(Fat_score),
+    Last_Score = last(Fat_score),
+    Slope = (Last_Score - First_Score) / as.numeric(Last_Date - First_Date),
+    .groups = "drop"
+  )
+
+fatslopes_with_strategy <- fatting_slopes %>%
+  left_join(video_data %>% dplyr::select(Three_letter_code, Strategy) %>% distinct(), by = "Three_letter_code")  |>
+  filter(!(Three_letter_code %in% c("JJJ", "JYL")))
+## birds with a slope of 0 removed
+
+ggplot(fatslopes_with_strategy, aes(x = Strategy, y= Slope, 
+                                    fill = Strategy)) +
+  geom_boxplot() +
+  scale_fill_manual(values = c("#E777F2", "#4DD2A4", "#4DC8F9")) +
+  labs(title = "Distribution of Fatting Slopes by Strategy",
+       x = "Slope (Fatting Up per Day)",
+       y = "Density") +
+  theme_minimal() +
+  theme(legend.position = "bottom")
+
+### removing outlier (in two days fatting up from 2.5 to 5, not possible)
+fatslopes_with_strategy <- fatslopes_with_strategy |>
+  filter(!Three_letter_code == "KTN")
+
+ggplot(fatslopes_with_strategy, aes(x = Slope)) +
+  geom_histogram(bins = 30, fill = "#69b3a2", color = "black") +
+  theme_minimal()
+
+mean(fatslopes_with_strategy$Slope)
+var(fatslopes_with_strategy$Slope)
+
+glm1 <- glm(Slope ~ 1,
+              family = inverse.gaussian(link = "log"),
+              data = fatslopes_with_strategy)
+
+glm2 <- glm(Slope ~ Strategy,
+              family = inverse.gaussian(link = "log"),
+              data = fatslopes_with_strategy)
+summary(glm2)
+
+glm3 <- glm(Slope ~ First_Date * Strategy,
+              family = inverse.gaussian(link = "log"),
+              data = fatslopes_with_strategy)
+summary(glm3)
+
+glm4 <- glm(Slope ~ First_Date + Strategy,
+              family = inverse.gaussian(link = "log"),
+              data = fatslopes_with_strategy)
+
+glm5  <- glm(Slope ~ Last_Date + Strategy,
+              family = inverse.gaussian(link = "log"),
+              data = fatslopes_with_strategy)
+
+glm6 <- glm(Slope ~ First_Date + Last_Date + Strategy,
+              family = inverse.gaussian(link = "log"),
+              data = fatslopes_with_strategy)
+
+model.sel(glm1, glm2, glm3, glm4, glm5, glm6)
+anova(glm1, glm2, glm3, glm4, glm5, glm6, test = "Chisq")
+
+emm_fat <- emmeans(glm4, ~ Strategy, type = "response")
+pairs(emm_fat)
+library(multcomp)
+
+cld(emm_fat, Letters = letters, adjust = "tukey")
+
+
+
